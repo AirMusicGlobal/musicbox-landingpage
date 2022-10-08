faf5d1ac941ef9b2e03a30f4da523a339db80bb1
Ivan Rendulić <ivan.rendulic@gmail.com>
2022-10-08 12:02:05 +0200

    Fix Submit...

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,9 @@
-
-    $('#toggle').click(function () {
-        $(this).toggleClass('active');
-        $('#overlay').toggleClass('open');
-    });
-$('.overlay-menu a').click(function () {
-    $('.button_container').toggleClass('active');
-    $('#overlay').toggleClass('open');
-    return true;
-}); 
+$("#toggle").click(function () {
+  $(this).toggleClass("active");
+  $("#overlay").toggleClass("open");
+});
+$(".overlay-menu a").click(function () {
+  $(".button_container").toggleClass("active");
+  $("#overlay").toggleClass("open");
+  return true;
+});
